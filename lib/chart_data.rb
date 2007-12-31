55c2ea9b00f1627d732180a4618a18c2be7b16b0
Pedro Côrte-Real <pedro@pedrocr.net>
2007-12-31 17:22:40 +0000

    Implement data preparing functions for charts.

---
+++ lib/chart_data.rb
@@ -0,0 +1,28 @@
+module SAAL
+  class ChartData
+    def initialize(opts = {})
+      @connection = opts[:connection] || Connection.new
+    end
+    
+    def get_data(sensor, from, to, num)
+      step = (to - from).to_f/num.to_f
+      (0..num-2).map do |i|
+        f = (from+i*step).to_i
+        t = (from+(i+1)*step-0.5).to_i
+        @connection.average(sensor, f, t)
+      end << @connection.average(sensor, (from+(num-1)*step).to_i, to)
+    end
+    
+    def normalize_data(data, min, max)
+      data.map do |i|
+        if i < min
+          0.0
+        elsif i > max
+          100.0
+        else
+          v = (((i-min)/(max-min).to_f)*1000).round/10.0
+        end
+      end
+    end
+  end
+end
---
+++ test/chart_data_test.rb
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+require 'time'
+
+MOCK_AVERAGES = [40,30,50,60,300]
+MOCK_MAX = 215.3
+MOCK_MIN = 35.2
+NORMALIZED_MOCK_AVERAGES = [2.7,0.0,8.2,13.8,100.0]
+
+class MockConnection
+  attr_reader :asked_averages
+  def initialize
+    @averages = MOCK_AVERAGES.dup
+    @asked_averages = []
+  end
+  def average(sensor, from, to) 
+    @asked_averages << [from,to]; 
+    @averages.shift
+  end
+end
+
+class TestChartData < Test::Unit::TestCase
+  def test_get_data
+    conn = MockConnection.new
+    c = SAAL::ChartData.new(:connection => conn)
+    assert_equal MOCK_AVERAGES, c.get_data('fake_temp', 0, 1000, 5)
+    assert_equal([[0,199],[200,399],[400,599],[600,799],[800,1000]],
+                 conn.asked_averages)
+  end
+  
+  def test_normalize_data
+    conn = MockConnection.new
+    c = SAAL::ChartData.new(:connection => conn)
+    d = c.get_data('fake_temp', 0, 1000, 5)
+    assert_equal NORMALIZED_MOCK_AVERAGES, 
+                 c.normalize_data(d, MOCK_MIN, MOCK_MAX)
+    assert_equal([[0,199],[200,399],[400,599],[600,799],[800,1000]],
+                 conn.asked_averages)    
+  end
+end
