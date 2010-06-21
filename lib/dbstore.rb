408ff15b347fa9adde3a7c52e9135d89b92a83a6
Pedro Côrte-Real <pedro@pedrocr.net>
2010-06-21 18:26:30 +0100

    First working version of the mysql store.

--- lib/dbstore.rb
+++ lib/dbstore.rb
@@ -4,46 +4,49 @@ module SAAL
   class DBStore
     def initialize(dbopts, initialize=false)
       @dbopts = dbopts
-      initialize_db if initialize
+      db_initialize if initialize
     end
 
-    def initialize_db
-      db.query("CREATE TABLE IF NOT EXISTS sensor_reads
-                  (sensor VARCHAR(100) INDEX, 
-                   date TIMESTAMP INDEX, 
-                   value FLOAT)")
+    def db_initialize
+      @db.query "CREATE TABLE IF NOT EXISTS sensor_reads
+                   (sensor VARCHAR(100), 
+                    date INT, 
+                    value FLOAT)"
     end
     
+    def db_wipe
+      @db.query "DROP TABLE sensor_reads"
+    end
+
     def write(sensor, date, value)
-      db.query("INSERT INTO sensor_reads VALUES"+
-                 "("+db.quote(sensor.to_s)+","+date.to_s+","+value.to_s+")"
+      @db.query "INSERT INTO sensor_reads VALUES
+                  ('"+@db.quote(sensor.to_s)+"',"+date.to_s+","+value.to_s+")"
     end
     
     def average(sensor, from, to)     
-      r = db.query("SELECT AVG(value) AS average FROM sensor_reads
-                      WHERE sensor = #{db.quote(sensor.to_s)} 
-                        AND date >= #{from.to_s} 
-                        AND date <= #{to.to_s}")
+      r = @db.query "SELECT AVG(value) AS average FROM sensor_reads
+                       WHERE sensor = '#{@db.quote(sensor.to_s)}' 
+                         AND date >= #{from.to_s} 
+                         AND date <= #{to.to_s}"
       r.num_rows > 0 ? r.fetch_row[0] : nil
     end
+
     
     # Add database connection to methods
-    [:initialize_db, :write, :average].each do |m|
+    [:db_initialize, :db_wipe, :write, :average].each do |m|
       old = instance_method(m)
       define_method(m) do |*args|
         ret = nil 
         begin
           # connect to the MySQL server
-          db = Mysql.new(@dbopts[:host],@dbopts[:user],@dbopts[:pass],
-                         @dbopts[:db],@dbopts[:port],@dbopts[:socket],
-                         @dbopts[:flags])
-          ret = old.bind(self).call(db,*args)
+          @db = Mysql.new(@dbopts[:host],@dbopts[:user],@dbopts[:pass],
+                          @dbopts[:db],@dbopts[:port],@dbopts[:socket],
+                          @dbopts[:flags])
+          ret = old.bind(self).call(*args)
         rescue Mysql::Error => e
-          puts "Error code: #{e.errno}"
-          puts "Error message: #{e.error}"
-          puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
+          $stderr.puts "MySQL Error #{e.errno}: #{e.error}"
         ensure
-          db.close if dbh
+          @db.close if @db
         end
         ret
       end
--- test/dbstore_test.rb
+++ test/dbstore_test.rb
@@ -1,41 +1,44 @@
 require File.dirname(__FILE__)+'/test_helper.rb'
 
 class TestFileStore < Test::Unit::TestCase
+  TEST_DBOPTS = {:host => 'localhost',
+                 :user => 'sensor_reads',
+                 :pass => 'abcd',
+                 :db => 'sensor_reads_test'}
+  TEST_DBARGS = [TEST_DBOPTS[:host],TEST_DBOPTS[:user],
+                 TEST_DBOPTS[:pass],TEST_DBOPTS[:db]]
+
+
   def setup
-    @fstore = SAAL::FileStore.new(:db => TEST_DBFILE)
+    @dbstore = SAAL::DBStore.new(TEST_DBOPTS, true)
+    @dbstore.db_wipe
+    @dbstore.db_initialize
   end
 
   def test_insert
     test_time = 1196024160
     test_value = 7.323
     
-    @fstore.write(:test_sensor, test_time, test_value)
-    @fstore.close
+    @dbstore.write(:test_sensor, test_time, test_value)
     
-    db = SQLite3::Database.new(TEST_DBFILE)
-    db.type_translation = true
-    $-w = false # disable sqlite warning message
-    rows = db.execute("select * from sensor_reads")
-    $-w = true
-    assert_equal [["test_sensor", test_time, test_value]], rows
+    db = Mysql.new(*TEST_DBARGS)
+    res = db.query("SELECT * FROM sensor_reads")
+    assert_equal 1, res.num_rows
+    assert_equal ["test_sensor", test_time.to_s, test_value.to_s], res.fetch_row
   end
   
   def test_average
     test_values = [[10, 7.323],[12, 5.432],[23, -2.125], [44, 0.123]]
     test_average = (5.432 - 2.125)/2.0
     test_values.each do |values|
-      @fstore.write(:test_sensor, *values)
+      @dbstore.write(:test_sensor, *values)
     end
     
-    assert_in_delta test_average, @fstore.average(:test_sensor, 11, 25), 0.001
-    assert_in_delta test_average, @fstore.average(:test_sensor, 12, 25), 0.001
-    assert_in_delta test_average, @fstore.average(:test_sensor, 12, 23), 0.001
+    assert_in_delta test_average, @dbstore.average(:test_sensor, 11, 25), 0.001
+    assert_in_delta test_average, @dbstore.average(:test_sensor, 12, 25), 0.001
+    assert_in_delta test_average, @dbstore.average(:test_sensor, 12, 23), 0.001
     
     # when there are no points it's nil
-    assert_nil @fstore.average(:test_sensor, 50, 60)
-  end
-  
-  def teardown
-    FileUtils.rm_f TEST_DBFILE
+    assert_nil @dbstore.average(:test_sensor, 50, 60)
   end
 end
