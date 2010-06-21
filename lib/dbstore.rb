62285046b3dd4ce8b95cd52bafe6634171c09beb
Pedro Côrte-Real <pedro@pedrocr.net>
2010-06-21 17:40:46 +0100

    First cut at converting the filestore to dbstore.

--- lib/dbstore.rb
+++ lib/dbstore.rb
@@ -1,47 +1,49 @@
-require 'sqlite3'
+require "mysql"
 
 module SAAL
-  class FileStore
-    def initialize(opts = {:db => "sensor_reads.db"})
-      @opts = opts
-      @db = SQLite3::Database.new(@opts[:db])
-      @db.type_translation = true
-      @db.execute("create table if not exists sensor_reads
-                    (sensor string, date integer, value real)")
-      @db.execute("create index if not exists sensor_reads_sensor_index on
-                    sensor_reads(sensor)")
-      @db.execute("create index if not exists sensor_reads_date_index on
-                    sensor_reads(date)")
-      @mutex = Mutex.new
+  class DBStore
+    def initialize(dbopts, initialize=false)
+      @dbopts = dbopts
+      initialize_db if initialize
+    end
+
+    def initialize_db
+      db.query("CREATE TABLE IF NOT EXISTS sensor_reads
+                  (sensor VARCHAR(100) INDEX, 
+                   date TIMESTAMP INDEX, 
+                   value FLOAT)")
     end
     
     def write(sensor, date, value)
-      @db.execute("insert into sensor_reads values (?,?,?)", 
-                  sensor.to_s, date, value)
+      db.query("INSERT INTO sensor_reads VALUES"+
+                 "("+db.quote(sensor.to_s)+","+date.to_s+","+value.to_s+")"
     end
     
-    def average(sensor, from, to)
-      r = @db.execute("select avg(value) as average from sensor_reads
-                        where sensor = ? and date >= ? and date <= ?",
-                       sensor.to_s, from, to)
-      if r[0][0]
-        Float(r[0][0])
-      else
-        nil
-      end
+    def average(sensor, from, to)     
+      r = db.query("SELECT AVG(value) AS average FROM sensor_reads
+                      WHERE sensor = #{db.quote(sensor.to_s)} 
+                        AND date >= #{from.to_s} 
+                        AND date <= #{to.to_s}")
+      r.num_rows > 0 ? r.fetch_row[0] : nil
     end
     
-    def close
-      @db.close
-    end
-    
-    # Guard operations that write to the database with a lock
-    [:write, :close].each do |m|
-    old = instance_method(m)
+    # Add database connection to methods
+    [:initialize_db, :write, :average].each do |m|
+      old = instance_method(m)
       define_method(m) do |*args|
         ret = nil 
-        @mutex.synchronize do
-          ret = old.bind(self).call(*args)
+        begin
+          # connect to the MySQL server
+          db = Mysql.new(@dbopts[:host],@dbopts[:user],@dbopts[:pass],
+                         @dbopts[:db],@dbopts[:port],@dbopts[:socket],
+                         @dbopts[:flags])
+          ret = old.bind(self).call(db,*args)
+        rescue Mysql::Error => e
+          puts "Error code: #{e.errno}"
+          puts "Error message: #{e.error}"
+          puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
+        ensure
+          db.close if dbh
         end
         ret
       end
