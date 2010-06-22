72457b853e3321b46f9708bf28e9ea1cdeb201b0
Pedro Côrte-Real <pedro@pedrocr.net>
2010-06-22 12:05:05 +0100

    Modify chart_data to use Sensor instead of Connection

--- lib/chart_data.rb
+++ lib/chart_data.rb
@@ -1,16 +1,16 @@
 module SAAL
   class ChartData
     def initialize(opts = {})
-      @connection = opts[:connection] || Connection.new
+      @sensor = opts[:sensor]
     end
     
-    def get_data(sensor, from, to, num)
+    def get_data(from, to, num)
       step = (to - from).to_f/num.to_f
       (0..num-2).map do |i|
         f = (from+i*step).to_i
         t = (from+(i+1)*step-0.5).to_i
-        @connection.average(sensor, f, t)
-      end << @connection.average(sensor, (from+(num-1)*step).to_i, to)
+        @sensor.average(f, t)
+      end << @sensor.average((from+(num-1)*step).to_i, to)
     end
     
     def normalize_data(data, min, max)
