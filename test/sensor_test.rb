3cbc70f65cb0dcffa6f3d5dd287fadeab4ce83df
Pedro Côrte-Real <pedro@pedrocr.net>
2010-06-23 18:52:32 +0100

    Refactor sensor tests.

--- test/sensor_test.rb
+++ test/sensor_test.rb
@@ -7,36 +7,43 @@ class MockConnection
   end
 end
 
+class MockDBStore
+  attr_accessor :value
+  def average(sensor, from, to)
+    @value
+  end
+end
+
 class TestSensor < Test::Unit::TestCase
   def setup
     @defs = YAML::load File.new(TEST_SENSOR_CLEANUPS_FILE)
     @conn = MockConnection.new
+    @dbstore = MockDBStore.new
+    @fake = SAAL::Sensor.new(@dbstore, 'fake', @defs['fake'], @conn)
+    @fake2 = SAAL::Sensor.new(@dbstore, 'fake', @defs['fake2'], @conn)
   end
 
   def test_read_too_high_values
     @conn.value = 1000
-    @sensor = SAAL::Sensor.new(nil, 'fake', @defs['fake'], @conn)
-    assert_nil @sensor.read
-    assert_nil @sensor.read_uncached
+    assert_nil @fake.read
+    assert_nil @fake.read_uncached
     @conn.value = 200
-    assert_equal 200, @sensor.read
-    assert_equal 200, @sensor.read_uncached
+    assert_equal 200, @fake.read
+    assert_equal 200, @fake.read_uncached
   end
 
   def test_read_too_low_values
     @conn.value = 0
-    @sensor = SAAL::Sensor.new(nil, 'fake', @defs['fake'], @conn)
-    assert_nil @sensor.read
-    assert_nil @sensor.read_uncached
+    assert_nil @fake.read
+    assert_nil @fake.read_uncached
     @conn.value = 200
-    assert_equal 200, @sensor.read
-    assert_equal 200, @sensor.read_uncached
+    assert_equal 200, @fake.read
+    assert_equal 200, @fake.read_uncached
   end
 
   def test_read_without_limits
     @conn.value = 200
-    @sensor = SAAL::Sensor.new(nil, 'fake', @defs['fake2'], @conn)
-    assert_equal 200, @sensor.read
-    assert_equal 200, @sensor.read_uncached
+    assert_equal 200, @fake2.read
+    assert_equal 200, @fake2.read_uncached
   end
 end
