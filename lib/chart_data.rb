9a9c2522094f969a52f767340b3eeb63643cddaa
Pedro Côrte-Real <pedro@pedrocr.net>
2010-12-30 19:36:15 +0000

    Stop depending on DateTime, no more monkeypatching

--- lib/chart_data.rb
+++ lib/chart_data.rb
@@ -1,33 +1,3 @@
-class Time
-  def to_datetime
-    # Convert seconds + microseconds into a fractional number of seconds
-    seconds = sec + Rational(usec, 10**6)
-
-    # Convert a UTC offset measured in minutes to one measured in a
-    # fraction of a day.
-    offset = Rational(utc_offset, 60 * 60 * 24)
-    DateTime.new(year, month, day, hour, min, seconds, offset)
-  end
-end
-
-class Date
-  def to_gm_time
-    to_time(new_offset, :gm)
-  end
-
-  def to_local_time
-    to_time(new_offset(DateTime.now.offset-offset), :local)
-  end
-
-  private
-  def to_time(dest, method)
-    #Convert a fraction of a day to a number of microseconds
-    usec = (dest.sec_fraction * 60 * 60 * 24 * (10**6)).to_i
-    Time.send(method, dest.year, dest.month, dest.day, dest.hour, dest.min,
-              dest.sec, usec)
-  end
-end
-
 module SAAL
   class ChartData
     def initialize(sensor)
@@ -86,32 +56,36 @@ module SAAL
       get_data(:average, num)
     end
 
+    private
     def self.calc_alignment(num, periods, now=nil)
       now ||= Time.now.utc
 
       if [:years, :year].include? periods
         # Calculate by date manipulation
-        from = Time.utc(now.year - num + 1, 1, 1, 0, 0, 0).to_i
-        to = Time.utc(now.year, 12, 31, 23, 59, 59).to_i
+        from = Time.utc(now.year - num + 1, 1, 1, 0, 0, 0)
+        to = Time.utc(now.year, 12, 31, 23, 59, 59)
       elsif [:months, :month].include? periods
+        # advance to the 1st of the next month and then subtract 1 second
         newm = now.month%12 + 1
         newy = now.year + (now.month == 12 ? 1 : 0)
-        to = Time.utc(newy, newm, 1, 0, 0, 0).to_i-1
+        to = Time.utc(newy, newm, 1, 0, 0, 0) - 1
         # subtract num months from a date
-        from = Time.at(to+1).to_datetime.<<(num).to_gm_time.to_i
+        newm = now.month-1 - (num-1)%12 + 1
+        newy = now.year - (num-1)/12
+        from = Time.utc(newy, newm, 1, 0, 0, 0, 0)
+        #from = (to+1).to_datetime.<<(num).to_gm_time
       else
         # Calculate by elasped time
         args = [now.year, now.month, now.day, now.hour, now.min, now.sec]
         args = args[0..-(ALIGN[periods].size+1)]
         args += ALIGN[periods]
-        to = Time.utc(*args).to_i
+        to = Time.utc(*args)
         to += (7-now.wday)*60*60*24 if [:weeks,:week].include?(periods)
         from = to - NUMHOURS[periods]*60*60*num+1
       end
-      [from, to]
+      [from.to_i, to.to_i]
     end
 
-    private
     def get_data(method, num)
       step = (@to - @from).to_f/num.to_f
       (0..num-2).map do |i|
