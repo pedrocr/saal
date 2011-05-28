668d6ef3126a1b44b84528a0331c5a6477695e1f
Pedro Côrte-Real <pedro@pedrocr.net>
2011-05-27 18:47:05 -0700

    Bump version

--- lib/saal.rb
+++ lib/saal.rb
@@ -10,7 +10,7 @@ module SAAL
     DBCONF = CONFDIR+"database.yml"
     CHARTSCONF = CONFDIR+"charts.yml"
 
-    VERSION = '0.2.11'
+    VERSION = '0.2.12'
 end
 
 require File.dirname(__FILE__)+'/dbstore.rb'
